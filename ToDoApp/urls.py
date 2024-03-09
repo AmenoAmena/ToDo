336785eb24e82e72b0134a74d1fb8b3b788469ca
AmenoAmena <ardacavusoglu666@gmail.com>
2024-03-09 14:57:13 +0300

    create ve read bitti

--- ToDoApp/urls.py
+++ ToDoApp/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.index,name='index')
+    path('',views.index,name='index'),
+    path('tasks',views.task_list,name = 'tasks')
 ]
--- ToDoApp/views.py
+++ ToDoApp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import Task
 from .forms import TaskForm
 
@@ -7,9 +7,14 @@ def index(request):
     if request.method == 'POST':
         task = request.POST.get('task')
         Task.objects.create(task = task )
-    tasks = Task.objects.all()
+        return redirect('tasks') 
     form = TaskForm()
     return render(request, 'ToDoApp/index.html',{
-        'tasks':tasks,
         'form':form  
+    })
+
+def task_list(request):
+    tasks = Task.objects.all()
+    return render(request, 'ToDoApp/task_list.html',{
+        'tasks':tasks
     })
