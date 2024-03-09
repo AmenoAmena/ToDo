66b1eff07891ea4c36e3be374ffd819b3c1fffec
AmenoAmena <ardacavusoglu666@gmail.com>
2024-03-09 20:33:20 +0300

    All crud done now styling needed but ı dont want to do

--- ToDoApp/urls.py
+++ ToDoApp/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('',views.index,name='index'),
     path('tasks',views.task_list,name = 'tasks'),
-    path('delete<int:id>',views.delete,name = 'delete')
+    path('delete<int:id>',views.delete,name = 'delete'),
+    path('update<int:id>',views.index,name='update')
 ]
--- ToDoApp/views.py
+++ ToDoApp/views.py
@@ -3,15 +3,26 @@ from .models import Task
 from .forms import TaskForm
 
 # Create your views here.
-def index(request):
-    if request.method == 'POST':
-        task = request.POST.get('task')
-        Task.objects.create(task = task )
-        return redirect('tasks') 
-    form = TaskForm()
-    return render(request, 'ToDoApp/index.html',{
-        'form':form  
-    })
+def index(request,id=0):
+    if request.method == 'GET':
+        if id==0:
+            form = TaskForm()
+        else:
+            task = Task.objects.get(pk=id)
+            form = TaskForm(instance=task)
+        return render(request, 'ToDoApp/index.html',{
+            'form':form  
+        })
+    else:
+        if id==0:
+            form = TaskForm(request.POST)
+        else:
+            task = Task.objects.get(pk=id)
+            form = TaskForm(request.POST,instance=task)
+        if form.is_valid():
+            form.save()
+        return redirect('tasks')
+
 
 def task_list(request):
     tasks = Task.objects.all()
